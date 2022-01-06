e993d4d8e8e7726c4436ba16d6024ff6cb2bdfb8
aggarwal-h <himanshu@preteckt.com>
2022-01-05 19:16:46 -0500

    Update package.json imports and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        dm: "'DM Sans', sans-serif",
+        inter: "'Inter', sans-serif",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+    },
   },
   plugins: [],
 };
