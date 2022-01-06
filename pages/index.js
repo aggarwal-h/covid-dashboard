5a505024b7ef3b5d15bbf30e990b9c2acc34b9f1
aggarwal-h <himanshu@preteckt.com>
2022-01-05 19:17:13 -0500

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import Head from "next/head";
+import Content from "../components/content/Content";
+import Sidebar from "../components/sidebar/Sidebar";
+import HomeComponent from "../components/home/Home";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <div>
+      <Head>
+        <title>COVID-19 Dashboard</title>
+      </Head>
+      <Sidebar />
+      <Content>
+        <HomeComponent />
+      </Content>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </div>
+  );
 }
