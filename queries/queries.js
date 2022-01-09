5c934611bbbc20f304caabb10ef55fd894de12c2
aggarwal-h <himanshu@preteckt.com>
2022-01-08 22:40:56 -0500

    Add more query hooks for API endpoints

--- queries/queries.js
+++ queries/queries.js
@@ -28,3 +28,77 @@ export function useDailyData() {
     }
   );
 }
+
+export function useDailyDataCountry(country) {
+  return useQuery(
+    `daily-${country}`,
+    () => {
+      return axios
+        .get(`http://localhost:3000/api/covid/${country}/timeseries/all`)
+        .then((res) => res.data);
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+}
+
+export function usePreviousDays(days) {
+  return useQuery(
+    `${days}-previous-days`,
+    () => {
+      return axios
+        .get(
+          `http://localhost:3000/api/covid/worldwide/timeseries/previous?days=${days}`
+        )
+        .then((res) => res.data);
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+}
+
+export function usePreviousDaysCountry(days, country) {
+  return useQuery(
+    `${days}-previous-days-${country?.toLowerCase()}`,
+    () => {
+      return axios
+        .get(
+          `http://localhost:3000/api/covid/${country}/timeseries/previous?days=${days}`
+        )
+        .then((res) => res.data);
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+}
+
+export function useCountriesToday() {
+  return useQuery(
+    `countries-today`,
+    () => {
+      return axios
+        .get(`http://localhost:3000/api/covid/country/timeseries/today`)
+        .then((res) => res.data);
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+}
+
+export function useAllCountriesInfo() {
+  return useQuery(
+    `all-countries-info`,
+    () => {
+      return axios
+        .get(`http://localhost:3000/api/country-info/all`)
+        .then((res) => res.data);
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+}
