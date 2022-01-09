77d0fca90404996101717c4b35acc9feedf3e591
aggarwal-h <himanshu@preteckt.com>
2022-01-08 23:31:34 -0500

    Update index to use new reusable components; Add country detail page

--- pages/[country].js
+++ pages/[country].js
@@ -1,9 +1,10 @@
+import React from "react";
 import Head from "next/head";
 import Content from "../components/content/Content";
 import Sidebar from "../components/sidebar/Sidebar";
 import Main from "../components/main/Main";
 
-export default function Home() {
+export default function CountryPage() {
   return (
     <div>
       <Head>
