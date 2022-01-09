a421473e4ae089d7ec9ede2cc669a70d9977753b
aggarwal-h <himanshu@preteckt.com>
2022-01-08 22:37:06 -0500

    Add wrapper for main content

--- components/content/Content.js
+++ components/content/Content.js
@@ -3,7 +3,9 @@ import React from "react";
 function Content({ children }) {
   return (
     <div className="main-content">
-      <div className="container mx-auto pt-8 p-16">{children}</div>
+      <div className="container mx-auto pt-8 p-2 sm:p-8 md:p-16">
+        {children}
+      </div>
     </div>
   );
 }
