4f39bdb83a102c6998022579d7e99bb75cd23eb1
aggarwal-h <himanshu@preteckt.com>
2022-01-08 22:28:30 -0500

    Make sidebar more dynamic and responsive

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -6,32 +6,56 @@ import {
   MapIcon,
 } from "@heroicons/react/outline";
 import SidebarItem from "./SidebarItem";
+import { useRouter } from "next/router";
 
 function Sidebar() {
+  const router = useRouter();
   return (
     <div className="sidebar">
       {/* Head */}
       <div className="sidebar-head">
         <a className="sidebar-logo">
-          <Image
-            className="sidebar-pic sidebar-pic-light"
-            src="/assets/img/covid_large.png"
-            width="150%"
-            height="100%"
-            objectFit="contain"
-            alt=""
-          />
+          <span className="hidden lg:block">
+            <Image
+              className="sidebar-pic sidebar-pic-light"
+              src="/assets/img/covid_large.png"
+              width="150%"
+              height="100%"
+              objectFit="contain"
+              alt=""
+            />
+          </span>
+          <span className="block lg:hidden">
+            <Image
+              className="sidebar-pic sidebar-pic-light"
+              src="/assets/img/covid.svg"
+              width="150%"
+              height="80%"
+              objectFit="contain"
+              alt=""
+            />
+          </span>
         </a>
-        <button>
-          <ChevronDoubleLeftIcon className="text-gray-600 w-6 h-6" />
+        <button className="mb-4 lg:mb-0">
+          <ChevronDoubleLeftIcon className="text-indigo-500 w-6 h-6" />
         </button>
       </div>
 
       {/* Body */}
       <div className="sidebar-body">
         <nav className="sidebar-nav">
-          <SidebarItem title="Home" icon={<HomeIcon />} active />
-          <SidebarItem title="Map" icon={<MapIcon />} />
+          <SidebarItem
+            title="Home"
+            icon={<HomeIcon />}
+            to="/"
+            active={router.pathname === "/"}
+          />
+          <SidebarItem
+            title="Map"
+            icon={<MapIcon />}
+            to="/map"
+            active={router.pathname === "/map"}
+          />
         </nav>
       </div>
     </div>
--- components/sidebar/SidebarItem.js
+++ components/sidebar/SidebarItem.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
-function SidebarItem({ title, icon, active }) {
+function SidebarItem({ title, icon, active, to }) {
   return (
-    <a className={`sidebar-item ${active ? "active" : ""}`}>
-      <div className="sidebar-icon">{icon}</div>
-      <div className="sidebar-text">{title}</div>
-    </a>
+    <Link href={to}>
+      <a className={`sidebar-item ${active ? "active" : ""}`}>
+        <div className="sidebar-icon">{icon}</div>
+        <div className="sidebar-text">{title}</div>
+      </a>
+    </Link>
   );
 }
 
