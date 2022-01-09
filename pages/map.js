ceb8e12d1e82c25063efc0410dda23a023b68fe9
aggarwal-h <himanshu@preteckt.com>
2022-01-08 22:27:02 -0500

    Add map page

---
+++ pages/map.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Content from "../components/content/Content";
+import Sidebar from "../components/sidebar/Sidebar";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+export default function Map() {
+  return (
+    <div>
+      <Head>
+        <title>COVID-19 Dashboard</title>
+      </Head>
+      <Sidebar />
+      <Content>
+        <h1>TODO</h1>
+      </Content>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </div>
+  );
+}
