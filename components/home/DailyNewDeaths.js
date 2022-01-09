aa44fac867f7987585c6f25fc78dc241f04b5859
aggarwal-h <himanshu@preteckt.com>
2022-01-08 22:38:26 -0500

    Update components to use the new reusable chart component

--- components/home/DailyNewDeaths.js
+++ components/home/DailyNewDeaths.js
@@ -1,18 +1,18 @@
 import React from "react";
 import TimeSeriesBarChart from "../charts/TimeSeriesBarChart";
 
-function DailyNewCases({ query }) {
+function DailyNewDeaths({ query }) {
   return (
-    <div className="card">
+    <div className="card mb-16">
       <div className="card-header">
-        <p className="text-xl font-poppins font-bold">Daily New Cases</p>
+        <p className="text-xl font-poppins font-bold">Daily New Deaths</p>
       </div>
       <div className="card-body">
         <TimeSeriesBarChart
           query={query}
-          color="#3990F3"
+          color="#FF7A68"
           timeDataName="date"
-          valueDataName="new_cases"
+          valueDataName="new_deaths"
           dateFormat="YYYY-MM-DD"
         />
       </div>
@@ -20,4 +20,4 @@ function DailyNewCases({ query }) {
   );
 }
 
-export default DailyNewCases;
+export default DailyNewDeaths;
