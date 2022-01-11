33de7290733686d0987972f6de46b39b874d3713
aggarwal-h <himanshu@preteckt.com>
2022-01-10 23:54:59 -0500

    Update query to use new materialized view; Update prisma schema

--- pages/api/covid/country/timeseries/today.js
+++ pages/api/covid/country/timeseries/today.js
@@ -2,34 +2,9 @@ import { prisma } from "../../../../../prisma/db";
 
 export default async function (req, res) {
   try {
-    // TODO: Fix below query for China's data
-    const result = await prisma.$queryRaw`
-      (SELECT
-        'World' as country,
-        'WO' as country_code,
-        date,
-        SUM(cumulative_cases) as cumulative_cases,
-        SUM(cumulative_deaths) as cumulative_deaths,
-        SUM(new_cases) as new_cases,
-        SUM(new_deaths) as new_deaths
-      FROM covid_timeseries
-      WHERE date = (SELECT MAX(date) FROM covid_timeseries)
-      GROUP BY date)
-      UNION
-      (SELECT
-        country_name as country,
-        country_alpha_2_code as country_code,
-        date,
-        SUM(cumulative_cases) as cumulative_cases,
-        SUM(cumulative_deaths) as cumulative_deaths,
-        SUM(new_cases) as new_cases,
-        SUM(new_deaths) as new_deaths
-      FROM covid_timeseries NATURAL JOIN countries
-      WHERE date = (SELECT MAX(date) FROM covid_timeseries)
-      GROUP BY country_name, country_alpha_2_code, date
-      )
-      ORDER BY cumulative_cases DESC
-    `;
+    // TODO: Fix below aggregated view for China's data
+    const result =
+      await prisma.$queryRaw`SELECT * FROM covid_country_aggregated_view`;
     res.status(200).json(result);
   } catch (e) {
     res.status(500).json({ error: "Unable to fetch all cases.", e });
