e665668e102c50063f8940c931b2a3d72af3386e
aggarwal-h <himanshu@preteckt.com>
2022-01-12 18:33:04 -0500

    Update map component to get sidebar state from cookies

--- pages/map.js
+++ pages/map.js
@@ -2,18 +2,27 @@ import Head from "next/head";
 import Content from "../components/content/Content";
 import Sidebar from "../components/sidebar/Sidebar";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { parseCookies } from "../utils";
+import BoldTitle from "../components/title/BoldTitle";
 
-export default function Map() {
+export default function Map({ initialSidebarMinimized }) {
   return (
     <div className="wrapper">
       <Head>
-        <title>COVID-19 Dashboard</title>
+        <title>COVID-19: Map</title>
       </Head>
-      <Sidebar />
+      <Sidebar initialSidebarMinimized={initialSidebarMinimized} />
       <Content>
-        <h1>TODO</h1>
+        <BoldTitle>This page is in development.</BoldTitle>
       </Content>
       <ReactQueryDevtools initialIsOpen={false} />
     </div>
   );
 }
+
+Map.getInitialProps = ({ req }) => {
+  const cookies = parseCookies(req);
+  return {
+    initialSidebarMinimized: cookies.sidebarMinimized,
+  };
+};
