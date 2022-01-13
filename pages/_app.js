d839b48fa6dbd78184693c9d80200d50492875a1
aggarwal-h <himanshu@preteckt.com>
2022-01-13 11:43:22 -0500

    Add progress bar to navigation

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import axios from "axios";
 import Script from "next/script";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const queryClient = new QueryClient();
 
